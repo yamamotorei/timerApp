987de32e84fd57cb8c255be731301f35cafa7599
“yamamotorei” <23iammvp@gmail.com>
2020-11-20 14:01:25 +0900

    first commit

---
+++ TimerApp/SettingViewController.swift
@@ -0,0 +1,24 @@
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBOutlet weak var timerSettingPicker: UIPickerView!
+    
+    
+    @IBAction func decisionButton(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+    
+}
---
+++ TimerApp/ViewController.swift
@@ -0,0 +1,80 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var countDownLabel: UILabel!
+    
+    //Timer型の「timer」という変数
+    var timer : Timer?
+    //変数countをInt型に指定
+    var count = 0
+    
+    let settingKey = "timer_value"
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //UserDefaultsのインスタンスを生成
+        let settings = UserDefaults.standard
+        //UserDefaultsに初期値を設定
+        settings.register(defaults: [settingKey:10])
+     
+    }
+
+
+    @IBAction func settingButton(_ sender: Any) {
+        if let nowTimer = timer {
+            if nowTimer.isValid == true {
+                nowTimer.invalidate()
+            }
+        }
+        performSegue(withIdentifier: "goSetting", sender: nil)
+    }
+    
+    
+    @IBAction func startButton(_ sender: Any) {
+        if let nowTimer = timer {
+            if nowTimer.isValid == true{
+                return
+            }
+        }
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerInterupt(_:)), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stopButton(_ sender: Any) {
+        if let nowTimer = timer {
+            if nowTimer.isValid == true{
+                nowTimer.invalidate()
+            }
+        }
+    }
+        func displayUpdate() -> Int {
+            let settings = UserDefaults.standard
+            let timerValue = settings.integer(forKey: settingKey)
+            let remainCount = timerValue - count
+            countDownLabel.text = "残り\(remainCount)　秒"
+            return remainCount
+        }
+    
+    @objc func timerInterupt(_ timer:Timer){
+        count += 1
+        if displayUpdate() <= 0{
+            count = 0
+            timer.invalidate()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        count = 0
+        
+        _ = displayUpdate()
+    }
+        
+    
+    
+    
+    
+}
+
